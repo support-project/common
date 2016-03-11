ae18b8f137e58b8cb3d49495b4070272a5015dff
Koda <koda.masaru3@gmail.com>
2016-03-11 10:03:15 +0900

    AppConfigが設定を読み込む際に、ベースディレクトリを環境変数で切り替えできるが、その環境変数のキーも設定できるようにした

--- src/main/java/org/support/project/common/config/AppConfig.java
+++ src/main/java/org/support/project/common/config/AppConfig.java
@@ -1,159 +1,239 @@
 package org.support.project.common.config;
 
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.support.project.common.log.Log;
+import org.support.project.common.log.LogFactory;
 import org.support.project.common.util.StringUtils;
+import org.support.project.common.util.SystemUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-
+/**
+ * アプリケーションの設定を保持するクラス
+ * 
+ * @author Koda
+ */
 public class AppConfig {
-	public static final String APP_CONFIG = "/appconfig.xml";
-	public static AppConfig get() {
-		if (appConfig == null) {
-			appConfig = ConfigLoader.load(AppConfig.APP_CONFIG, AppConfig.class);
-		}
-		return appConfig;
-	}
-	private static AppConfig appConfig = null;
+    /** ログ */
+    private static final Log LOG = LogFactory.getLog(AppConfig.class);
 
-	private String systemName;
-	
-	private String time_zone;
-	
-	private String basePath;
-	private boolean convBasePath = false;
+    /** 設定ファイルのパス */
+    public static final String APP_CONFIG = "/appconfig.xml";
+    /** インスタンス取得 */
+    public static AppConfig get() {
+        if (appConfig == null) {
+            LOG.info("Load App Congfig");
+            appConfig = ConfigLoader.load(AppConfig.APP_CONFIG, AppConfig.class);
+        }
+        return appConfig;
+    }
+    /**
+     * コンストラクタ
+     */
+    public AppConfig() {
+        LOG.info("AppConfig()");
+        if (StringUtils.isEmpty(AppConfig.envKey) && !initialize) {
+            // 初回の一回目だけ、AppConfig上のenvKeyを読み込む
+            LOG.info("Load envKey if exists.");
+            try {
+                Document document = DocumentBuilderFactory
+                        .newInstance()
+                        .newDocumentBuilder()
+                        .parse(AppConfig.class.getResourceAsStream(APP_CONFIG));
+                NodeList envs = document.getElementsByTagName("envKey");
+                if (envs.getLength() > 0) {
+                    Node env = envs.item(0);
+                    String envKey = env.getFirstChild().getNodeValue();
+                    LOG.info("env key: " + envKey);
+                    if (StringUtils.isNotEmpty(envKey)) {
+                        AppConfig.initEnvKey(envKey);
+                    }
+                }
+            } catch (SAXException | IOException | ParserConfigurationException e) {
+                LOG.error("initialize error.", e);
+            }
+            initialize = true;
+        }
+    }
+    /** 初期化したか */
+    private static boolean initialize = false;
+    
+    
+    /** インスタンス */
+    private static AppConfig appConfig = null;
+    /** システム名 */
+    private String systemName;
+    /** タイムゾーン */
+    private String time_zone;
+    /** システムのベースのパス */
+    private String basePath;
+    /** ベースパスの置換処理を実行したかどうかのフラグ */
+    private boolean convBasePath = false;
 
-	private String databasePath;
-	private boolean convDatabasePath = false;
-	
-	private String logsPath;
-	private boolean convLogsPath = false;
-	
-	private static boolean disp_env_info = false;
-	
-	/** ユーザのホームディレクトリ(BasePath)を指定する環境変数のキー */
-	private static String envKey = "";
-	public static void initEnvKey(String envKey) {
-		AppConfig.envKey = envKey;
-	}
-	
-	/**
-	 * パスの中に含まれる予約語を置換
-	 * @param path
-	 * @return
-	 */
-	public String convPath(String path) {
-		if (path.indexOf("{user.home}") != -1) {
-			String userHome = System.getProperty("user.home");
-			if (userHome.endsWith("/")) {
-				userHome = userHome.substring(0, userHome.length() -1);
-			}
-			path = path.replace("{user.home}", userHome);
-		}
-		if (path.indexOf("{base.path}") != -1) {
-			path = path.replace("{base.path}", getBasePath());
-		}
-		if (path.indexOf("\\") != -1) {
-			path = path.replaceAll("\\\\", "/");
-		}
-		return path;
-	}
+    /** システムの組み込みデータベースのパス */
+    private String databasePath;
+    /** 組み込みデータベースのパスを置換処理したかどかのフラグ */
+    private boolean convDatabasePath = false;
 
-	
-	/**
-	 * @return the time_zone
-	 */
-	public String getTime_zone() {
-		return time_zone;
-	}
+    /** システムのログのパス */
+    private String logsPath;
+    /** ログのパスを置換処理したかどかのフラグ */
+    private boolean convLogsPath = false;
+    
+    /** 環境変数をデバッグ出力したかどうかのフラグ(一度だけ出力する) */
+    private static boolean dispEnvInfo = false;
 
-	/**
-	 * @param time_zone the time_zone to set
-	 */
-	public void setTime_zone(String time_zone) {
-		this.time_zone = time_zone;
-	}
-	
-	
-	public String getDatabasePath() {
-		if (StringUtils.isEmpty(databasePath)) {
-			return "";
-		}
-		if (!convDatabasePath) {
-			String path = databasePath;
-			this.databasePath = convPath(path);
-			this.convDatabasePath = true;
-		}
-		return databasePath;
-	}
+    /** ユーザのホームディレクトリ(BasePath)を指定する環境変数のキー */
+    private static String envKey = "";
+    
+    /** 環境変数のキー文字列を設定 */
+    public static void initEnvKey(String envKey) {
+        if (StringUtils.isNotEmpty(AppConfig.envKey)) {
+            LOG.warn("envKey was already exists. [old]" + AppConfig.envKey + " [new]" + envKey);
+        }
+        AppConfig.envKey = envKey;
+    }
+    /** 環境変数のキー文字列を取得 */
+    public static String getEnvKey() {
+        return envKey;
+    }
 
-	public void setDatabasePath(String databasePath) {
-		this.databasePath = databasePath;
-	}
+    /**
+     * パスの中に含まれる予約語を置換
+     * 
+     * @param path
+     * @return
+     */
+    public String convPath(String path) {
+        if (path.indexOf("{user.home}") != -1) {
+            String userHome = System.getProperty("user.home");
+            if (userHome.endsWith("/")) {
+                userHome = userHome.substring(0, userHome.length() - 1);
+            }
+            path = path.replace("{user.home}", userHome);
+        }
+        if (path.indexOf("{base.path}") != -1) {
+            path = path.replace("{base.path}", getBasePath());
+        }
+        if (path.indexOf("\\") != -1) {
+            path = path.replaceAll("\\\\", "/");
+        }
+        return path;
+    }
 
-	/**
-	 * @return the basePath
-	 */
-	public String getBasePath() {
-		if (StringUtils.isEmpty(basePath)) {
-			return "";
-		}
-		if (!convBasePath) {
-			String path = basePath;
-			if (StringUtils.isNotEmpty(envKey)) {
-				String envValue = System.getenv(envKey);
-				if (StringUtils.isNotEmpty(envValue)) {
-					path = envValue;
-					if (!disp_env_info) {
-						System.out.println("Env [" + envKey + "] was loaded. value is [" + envValue + "].");
-						disp_env_info = true;
-					}
-				}
-			}
-			this.basePath = convPath(path);
-			this.convBasePath = true;
-		}
-		return basePath;
-	}
+    /**
+     * @return the time_zone
+     */
+    public String getTime_zone() {
+        return time_zone;
+    }
 
-	/**
-	 * @param basePath the basePath to set
-	 */
-	public void setBasePath(String basePath) {
-		this.basePath = basePath;
-	}
+    /**
+     * @param time_zone
+     *            the time_zone to set
+     */
+    public void setTime_zone(String time_zone) {
+        this.time_zone = time_zone;
+    }
+    
+    /**
+     * 組み込み用データベースのパスを取得
+     * @return
+     */
+    public String getDatabasePath() {
+        if (StringUtils.isEmpty(databasePath)) {
+            return "";
+        }
+        if (!convDatabasePath) {
+            String path = databasePath;
+            this.databasePath = convPath(path);
+            this.convDatabasePath = true;
+        }
+        return databasePath;
+    }
+    /**
+     * 組み込み用データベースのパスをセット
+     * @param databasePath
+     */
+    public void setDatabasePath(String databasePath) {
+        this.databasePath = databasePath;
+    }
 
-	/**
-	 * @return the systemName
-	 */
-	public String getSystemName() {
-		return systemName;
-	}
+    /**
+     * @return the basePath
+     */
+    public String getBasePath() {
+        if (StringUtils.isEmpty(basePath)) {
+            return "";
+        }
+        if (!convBasePath) {
+            String path = basePath;
+            if (StringUtils.isNotEmpty(envKey)) {
+                String envValue = SystemUtils.getenv(envKey);
+                if (StringUtils.isNotEmpty(envValue)) {
+                    path = envValue;
+                    if (!dispEnvInfo) {
+                        System.out.println("Env [" + envKey + "] was loaded. value is [" + envValue + "].");
+                        dispEnvInfo = true;
+                    }
+                }
+            }
+            this.basePath = convPath(path);
+            this.convBasePath = true;
+        }
+        return basePath;
+    }
 
-	/**
-	 * @param systemName the systemName to set
-	 */
-	public void setSystemName(String systemName) {
-		this.systemName = systemName;
-	}
+    /**
+     * @param basePath
+     *            the basePath to set
+     */
+    public void setBasePath(String basePath) {
+        this.basePath = basePath;
+    }
 
-	/**
-	 * @return the logsPath
-	 */
-	public String getLogsPath() {
-		if (StringUtils.isEmpty(logsPath)) {
-			return "";
-		}
-		if (!convLogsPath) {
-			String path = logsPath;
-			this.logsPath = convPath(path);
-			this.convLogsPath = true;
-		}
-		return logsPath;
-	}
+    /**
+     * @return the systemName
+     */
+    public String getSystemName() {
+        return systemName;
+    }
 
-	/**
-	 * @param logsPath the logsPath to set
-	 */
-	public void setLogsPath(String logsPath) {
-		this.logsPath = logsPath;
-	}
+    /**
+     * @param systemName
+     *            the systemName to set
+     */
+    public void setSystemName(String systemName) {
+        this.systemName = systemName;
+    }
+
+    /**
+     * @return the logsPath
+     */
+    public String getLogsPath() {
+        if (StringUtils.isEmpty(logsPath)) {
+            return "";
+        }
+        if (!convLogsPath) {
+            String path = logsPath;
+            this.logsPath = convPath(path);
+            this.convLogsPath = true;
+        }
+        return logsPath;
+    }
+
+    /**
+     * @param logsPath
+     *            the logsPath to set
+     */
+    public void setLogsPath(String logsPath) {
+        this.logsPath = logsPath;
+    }
 
 }
--- src/test/java/org/support/project/common/config/AppConfigTest.java
+++ src/test/java/org/support/project/common/config/AppConfigTest.java
@@ -1,6 +1,6 @@
 package org.support.project.common.config;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Locale;
 
@@ -12,54 +12,60 @@ import org.junit.Test;
 
 public class AppConfigTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        System.setProperty("APP_HOME", "/data/temp");
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testGetTime_zone() {
-		AppConfig config = ConfigLoader.load(AppConfig.APP_CONFIG, AppConfig.class);
-		assertEquals("Asia/Tokyo", config.getTime_zone());
-		
-	}
+    @Test
+    public void testGetTime_zone() {
+        AppConfig config = ConfigLoader.load(AppConfig.APP_CONFIG, AppConfig.class);
+        assertEquals("Asia/Tokyo", config.getTime_zone());
+    }
 
-	@Test
-	public void testSetTime_zone() {
-		AppConfig config = new AppConfig();
-		config.setTime_zone("America");
-		assertEquals("America", config.getTime_zone());
-	}
-	
-	
-	@Test
-	public void testGetConfigOnLocale() {
-		String dir = "/org/support/project/common/config/";
-		String file = "appconfig";
-		AppConfig config = LocaleConfigLoader.load(dir, file, Locale.CANADA, AppConfig.class);
-		assertEquals("CANADA", config.getTime_zone());
+    @Test
+    public void testBasePath() {
+        AppConfig appConfig = AppConfig.get();
+        assertEquals("/data/temp", appConfig.getBasePath());
+        assertEquals("/data/temp/logs/", appConfig.getLogsPath());
+    }
+    
+    
+    @Test
+    public void testSetTime_zone() {
+        AppConfig config = new AppConfig();
+        config.setTime_zone("America");
+        assertEquals("America", config.getTime_zone());
+    }
 
-		config = LocaleConfigLoader.load(dir, file, Locale.CANADA, AppConfig.class);
-		assertEquals("CANADA", config.getTime_zone());
+    @Test
+    public void testGetConfigOnLocale() {
+        String dir = "/org/support/project/common/config/";
+        String file = "appconfig";
+        AppConfig config = LocaleConfigLoader.load(dir, file, Locale.CANADA, AppConfig.class);
+        assertEquals("CANADA", config.getTime_zone());
 
-		config = LocaleConfigLoader.load(dir, file, Locale.JAPAN, AppConfig.class);
-		assertEquals("東京", config.getTime_zone());
+        config = LocaleConfigLoader.load(dir, file, Locale.CANADA, AppConfig.class);
+        assertEquals("CANADA", config.getTime_zone());
 
-		config = LocaleConfigLoader.load(dir, file, Locale.FRANCE, AppConfig.class);
-		assertEquals("FRANCE", config.getTime_zone());
-	
-	}
-	
+        config = LocaleConfigLoader.load(dir, file, Locale.JAPAN, AppConfig.class);
+        assertEquals("東京", config.getTime_zone());
+
+        config = LocaleConfigLoader.load(dir, file, Locale.FRANCE, AppConfig.class);
+        assertEquals("FRANCE", config.getTime_zone());
+
+    }
 
 }
