5c0a5b7478221808fd0e211b604c390d7a00fe59
Koda <koda.masaru3@gmail.com>
2016-04-13 22:53:10 +0900

    add class for exception handle

---
+++ src/main/java/org/support/project/common/exception/ExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.support.project.common.exception;
+
+public class ExceptionHandler {
+
+    /**
+     * Search exception for root causes
+     * @param e Throwable
+     * @param type search type
+     * @return exception
+     */
+    public static <T> T exceptionSearch(Throwable e, Class<? extends Exception> type) {
+        if (type.isAssignableFrom(e.getClass())) {
+            return (T) e;
+        }
+        if (e.getCause() == null) {
+            return null;
+        }
+        return exceptionSearch(e.getCause(), type);
+    }
+}
